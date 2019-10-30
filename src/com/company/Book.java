4c447a95f91cfccdc196973ae700d771dc90e1fd
yusufr98 <yusuf090798@gmail.com>
2019-10-30 15:57:49 -0400

    initial

---
+++ src/com/company/Book.java
@@ -0,0 +1,66 @@
+package com.company;
+
+public class Book {
+    private String title;
+    private String author;
+    private String description;
+    private double price;
+    private boolean isInStock;
+
+    public Book(String title, String author, String description, double price, boolean isInStock) {
+        this.title = title;
+        this.author = author;
+        this.description = description;
+        this.price = price;
+        this.isInStock = isInStock;
+    }
+
+    public Book() {
+    }
+
+    public void getDisplayText(){
+        System.out.println("Title: " + this.title);
+        System.out.println("Author: " + this.author);
+        System.out.println("Description: " + this.description);
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public boolean isInStock() {
+        return isInStock;
+    }
+
+    public void setInStock(boolean inStock) {
+        isInStock = inStock;
+    }
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    Book book = new Book("Harry Potter", "JK Rowling", "Seemingly regular orphan has world turned upsoide down when he finds out he is actually a famous wizard.", 9.99, true);
+        book.getDisplayText();
+    }
+}
